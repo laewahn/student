6a09c145d65f90d4c7ac08795552040452adff5f
Dennis Lewandowski <dennis@cs.rwth-aachen.de>
2012-12-17 16:25:27 +0100

    Added course user relation on model layer

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,6 @@
 class Course < ActiveRecord::Base
   attr_accessible :about, :field, :name, :semester
+  
+  has_and_belongs_to_many :users
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   attr_accessible :hashed_password, :home_country, :major, :name, :salt, :semesters, :user_name
   
   belongs_to :university
+  has_and_belongs_to_many :courses
   
 end
