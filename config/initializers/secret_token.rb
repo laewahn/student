7ef1235f4c6d995f1f085319741adc8a46d3175d
Dennis Lewandowski <dennis@cs.rwth-aachen.de>
2012-12-17 15:22:54 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Student::Application.config.secret_token = 'f50b57fa88cf6b70df177dcf901e897cd6f78016a15eb898ceeccd7f0874cde1a5625488c198754c086f3d5df80981b6fe979ea8b072f256add22a76d27812c1'
